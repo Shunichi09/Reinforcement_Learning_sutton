e8ff1a16a3cb5d110552e3911484401445e353a5
Shunichi09 <quick1st97of@gmail.com>
2018-08-09 10:07:31 +0900

    add

---
+++ 5th/blackjack1.py
@@ -0,0 +1,23 @@
+# ブラックジャック
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Blackjack():
+    def __init__(self):
+        self.dealer = None
+        self.player = None
+        self.ace = None
+        self.analyzer = montecalro
+
+    def play(self):
+        while end_flag:
+
+
+
+    def draw(self)
+
+
+cla
+
+
