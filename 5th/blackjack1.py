4da1d3d3eecf289c1df48c4894b709b82cb76f16
Shunichi09 <quick1st97of@gmail.com>
2018-08-12 18:00:41 +0900

    add

--- 5th/blackjack1.py
+++ 5th/blackjack1.py
@@ -2,12 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def cards_range_correcter(card): # 引いたカードはここ通す
-    if card == 1:
-        card = 11
-
-    if card > 10:
+def card_range_correcter(card): # 引いたカードはここ通す
+    if card > 10: # 絵札排除
         card = 10
+    
+    if card == 1: # とりあえずAceは11で計算
+        card = 11
     
     return card
 
@@ -24,56 +24,90 @@ class Dealer(): # ディーラーの場合
 
         while not stop_flag:
             self._draw()
-            stop_flag = self._judge_stop()          
+            stop_flag, score = self._judge_stop()          
 
-        return self.cards, score # カードの履歴と和を返す
+        return self.open, score # 開いてるカードの履歴と最終的なscoreを返す　ディラーなのでこれだけでいい
 
     def _judge_stop(self): # ゲーム終了かどうか判断
         # 初期化
         stop_flag = False
+
         # 和をとる
         score = sum(self.cards)
+
         # 大きさ確認
-        if self.sum > 21:# 21より大きい場合
-            self.cards
+        while 11 in self.cards and score > 21: # 21より大きくてAceを利用する場合
+            self.cards[self.cards.index(11)] = 1 # 1を代入
+            score = sum(self.cards)
 
-
-        if self.sum >= 17:
+        # 17以上だとdealerはストップ
+        if score >= 17:
             stop_flag = True
 
-        return stop_flag
+        return stop_flag, score
 
     def _draw(self): # カードを引く
         draw_card = card_range_correcter(np.random.randint(1, 13))
-        self.cards.append()
+        self.cards.append(draw_card)
 
-
-class Player(): # player
+class Player(): # playerの場合
     def __init__(self):
-        self.cards = [] # 持っているカード
+        self.cards = [] # 持っているカード これは最初2枚もってるから連動しないはず　下2つは個数合うはず
+        # 持っているカードの和の推移（状態の推移）
+        self.state_traj = []
+        # Aceのtraj
+        self.traj_Ace_flag = []
+    
+    def play(self):
+        stop_flag = self._judge_stop()
 
-   def _judge_stop(self): # ゲーム終了かどうか判断
+        while not stop_flag:
+            self._draw()
+            stop_flag, Ace_flag, score = self._judge_stop()          
+
+        return Ace_flag, self.state_traj, score # Aceを11として使用したか、状態推移、最終的なscore
+
+    def _judge_stop(self): # ゲーム終了かどうか判断
         # 初期化
         stop_flag = False
+
         # 和をとる
-        self.sum = sum(self.cards)
+        score = sum(self.cards)
+
         # 大きさ確認
-        if self.sum > 21:# 21より大きい場合
-            self.cards
+        while 11 in self.cards and score > 21: # 21より大きくてAceを利用する場合
+            self.cards[self.cards.index(11)] = 1 # 1を代入
+            score = sum(self.cards)
 
-        if self.sum >= 17:
+        # 履歴に状態追加
+        self.state_traj.append(score)
+        
+        # 20以上だとplayerはストップ
+        if score >= 20:
             stop_flag = True
+        
+        # ここでAceを利用しているか判定
+        if 11 in self.cards:
+            Ace_flag = True
+        else:
+            Ace_flag = False
+        
+        # 履歴に追加しておく
+        self.traj_Ace_flag.append(Ace_flag)
 
-        return stop_flag
+        return stop_flag, Ace_flag, score
 
     def _draw(self): # カードを引く
         draw_card = card_range_correcter(np.random.randint(1, 13))
-        self.cards.append()
+        self.cards.append(draw_card)
 
 class Blackjack():
     def __init__(self):
         # モンテカルロのやつ保存する
-        self.value_state = np.array([])
+        # 3次元配列
+        # 1→Aceあり
+        # 0→Aceなし
+        self.value_state = np.zeros((2, 10, 10))
 
         # 各プレイヤー定義
         self.dealer = Dealer()
@@ -81,52 +115,38 @@ class Blackjack():
     
     def play(self):
         # まずはディーラー
-
+        open_card, dealer_score = self.dealer.play()
 
         # 次にプレイヤー
-
+        traj_Ace_flag, state_traj, player_score = self.player.play()
 
         # judge
-
-
-        # 価値計算します
-
+        reward = self._reward(dealer_score, player_score)
 
         # 保存
+
         
-    def _reward(self): #価値計算
-        if player_score > dealer_score:
+    def _reward(self, dealer_score, player_score): #価値計算
+        if player_score > dealer_score: # player勝ち
             reward = 1
-        elif player_score < dealer_score:
+        elif player_score < dealer_score: # dealer勝ち
             reward = -1
         else:
-            reward = 0
+            reward = 0 # 引き分け
 
         return reward        
 
-    def _judge_game(self): #勝ち負け
-
-
-
-        return end_flag
-    
-    def 
-
-
-
 def main():
-    # saveするための！
-    
+    game = Blackjack()
 
     iterations = 10000
-    
+
     for i in range(iterations):
-        game = Blackjack()
-        while end_flag:
+        state_value = game.play()
 
-            game.draw()
-            game.play()
+    # 3Dplot
 
+if __name__ == '__main__':
+    main()
 
-            game.save()
 
