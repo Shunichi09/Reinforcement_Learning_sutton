a46cea68a3b5d64594fc1ed9f78bfaf6ed3d009b
Shunichi09 <quick1st97@gmail.com>
2018-11-05 11:16:13 +0900

    done TD learning

--- 6th/practice_1/random_walk.py
+++ 6th/practice_1/random_walk.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from collections import OrderedDict
 
 class RandomWalk():
     """
     environment model "Random model"
     Attributes
     ------------
-    state : 
-
+    states : list of str
+        state of environment
     """
     
     def __init__(self):
-        self.states = ["A", "B", "C", "D", "E"]
-        self.action = None
+        self.states =  ["left_goal", "A", "B", "C", "D", "E", "right_goal"] 
 
     def state_update(self, state, action):
         """
@@ -32,80 +32,108 @@ class RandomWalk():
             temporal (meaning not total) reward 
         """
         end_flg = False
-        temp_reward = 0         
+        temp_reward = 0.         
 
         if action == "left":
             next_state_num = self.states.index(state) - 1
-            if next_state_num < 0:
+            if self.states[next_state_num] == "left_goal":
                 end_flg = True
-                next_state = None
-                temp_reward = 0 # end the game
-                
+                temp_reward = 0. # end the game
+            
             next_state = self.states[next_state_num]
 
         elif action == "right":
             next_state_num = self.states.index(state) + 1
-            if next_state_num >= len(self.states):
-                end_flg = True
-                next_state = None
-                temp_reward = 1 # clear the game
 
+            if self.states[next_state_num] == "right_goal":
+                end_flg = True
+                temp_reward = 1. # clear the game
+        
             next_state = self.states[next_state_num]
 
         return end_flg, next_state, temp_reward
 
 class Agent():
     """
+    environment model "Random model"
+    Attributes
+    ------------
+    state : str 
+        now state of agent
+    states : list of str
+        state of environment
+    action_list : list of str
+        agent possibile action list 
+    step rate : float
+        learning rate of reinforcement learning
     """
 
-    def __init__(self, init):
-        self.state = None
-        self.states = ["A", "B", "C", "D", "E"] # in this case we shoud know the all states
-        self.action_list = ["left", "right"]
-        self.step_rate = 0.1 # adjust subject
-        
-        self.values = {}
-        for state in self.states:
-            self.values[state] = 0.5
-        
+    def __init__(self):
+        # environment
         self.model = RandomWalk()
+        
+        self.state = None
+        self.states = self.model.states # in this case we shoud know the all states
+        self.action_list = ["left", "right"]
+
+        self.step_rate = 0.1 # adjust subject
+        self.discount_rate = 1.0
         
         # for bootstrap
         self.history_state = None
+        self.values = OrderedDict()
+        for state in self.states:
+            self.values[state] = 0.5
+
+            if state == "left_goal": # in this problem the final state does not have value
+                self.values[state] = 0.
+            if state == "right_goal":
+                self.values[state] = 0.
 
     def train(self, train_num):
-        """
+        """training the agent
         Parameters
         -----------
         train_num : int
-
+            training number of reinforcement learning
         """
         for _ in range(train_num):
             self.state = "C" # initial state has been decided
             self.history_state = []
 
-            while not end_flg: # Ending this while is worth of ending one episode
+            while True: # Ending this while is worth of ending one episode
 
-                self.play()
+                end_flg, next_state, temp_reward = self._play(self.state) # play the game
                 # for TD
-                self._valuefunc_update()
+                self._valuefunc_update(temp_reward, self.state, next_state)
 
+                # save the state and update
+                self.history_state.append(self.state)
+                self.state = next_state
+
+                if end_flg:
+                    break
+                
             # for montecarlo
-            self._valuefunc_update()
+            # self._valuefunc_update()
 
-    def play(self):
+    def _play(self, state):
         """
+        playing the game
+
+        Parameters
+        -----------
+        state : str
+            state of the agent
         """
-        
         action = self._decide_action()
-
-        end_flg, next_state, temp_reward = self.model.state_update(self.state, action)
+        end_flg, next_state, temp_reward = self.model.state_update(state, action)
 
         # save the state for montecarlo
-        if state not in self.history_state: # ES
-            self.history_state.append(self.state)
+        # if state not in self.history_state: # ES
+        #    self.history_state.append(self.state)
 
-        self.state = next_state
+        return end_flg, next_state, temp_reward
 
     def _decide_action(self):
         """
@@ -115,25 +143,53 @@ class Agent():
             action of agent
         """
 
-        action = random.choice(action_list)
+        action = random.choice(self.action_list)
 
         return action
 
-    def _valuefunc_update(self):
+    def _valuefunc_update(self, reward, state, next_state):
         """
+        Parameters
+        -----------
+        reward : int
+            reward the agent got
+        state : str 
+            now state of agent
+        next_state : str 
+            next state of agent
         """
-        # montecarlo
-        for state in self.history_state:
-            values[state] = values[state] + self.step_rate * (final_reward - values[state])
-
         # TD
-        values[state] = values[state] + self.step_rate * (reward + self.discount_rate * values[next_state] - values[state])
+        self.values[state] = self.values[state] + self.step_rate * (reward + self.discount_rate * self.values[next_state] - self.values[state])
 
 def main():
+    # 0
+    agent = Agent()
+    agent.train(0)
+    values = list(agent.values.values())
+    plt.plot(range(len(agent.values.keys()) - 2), values[1:-1] , label="0", marker=".")
+    # 1
+    agent = Agent()
+    agent.train(1)
+    values = list(agent.values.values())
+    plt.plot(range(len(agent.values.keys()) - 2), values[1:-1] , label="1", marker=".")
+    # 10
+    agent = Agent()
+    agent.train(10)
+    values = list(agent.values.values())
+    plt.plot(range(len(agent.values.keys()) - 2), values[1:-1] , label="10", marker=".")
+    # 100
+    agent = Agent()
+    agent.train(100)
+    values = list(agent.values.values())
+    plt.plot(range(len(agent.values.keys()) - 2), values[1:-1] , label="100", marker=".")
+    # true
+    values = np.arange(1/6, 0.9, 1/6)
+    print(values)
+    plt.plot(range(len(agent.values.keys()) - 2), values, label="True", marker=".")
 
-
-
-
+    plt.legend()
+    plt.show()
+    
 
 if __name__ == '__main__':
     main()
